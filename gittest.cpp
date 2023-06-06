f785e342df6a7d600d10abc5fbdcf1a865d9f2c5
Haoqi Zhao <1471887278@qq.com>
2023-06-06 10:23:03 +0800

    main

--- gittest.cpp
+++ gittest.cpp
@@ -30,3 +30,28 @@ void quickSort(int *arr, int left, int right)
     quickSort(arr, left, i - 1);
     quickSort(arr, i + 1, right);
 }
+//写出归并排序的代码
+void mergeSort(int *arr, int left, int right)
+{
+    if (left >= right)
+        return;
+    int mid = (left + right) >> 1;
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    int i = left, j = mid + 1, k = 0;
+    int *temp = new int[right - left + 1];
+    while (i <= mid && j <= right)
+    {
+        if (arr[i] <= arr[j])
+            temp[k++] = arr[i++];
+        else
+            temp[k++] = arr[j++];
+    }
+    while (i <= mid)
+        temp[k++] = arr[i++];
+    while (j <= right)
+        temp[k++] = arr[j++];
+    for (int i = left, k = 0; i <= right; i++, k++)
+        arr[i] = temp[k];
+    delete[] temp;
+}
